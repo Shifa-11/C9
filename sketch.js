6f102c13b45b3d1834b6cd4fe3f2e04a7c518d89
Shifa-11 <92002928+Shifa-11@users.noreply.github.com>
2021-10-05 19:01:01 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box; 
+function setup() {
+  createCanvas(1000,1000);
+  fill("blue");
+box = createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(250);
+  drawSprites()
+}
+
+
+
+
